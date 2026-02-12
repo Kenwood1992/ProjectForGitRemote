d42f2d5bbf529db8df8746c29a99d35a30aa2b2d
Артур Султанов <artur9sultanov@mail.ru>
2026-02-13 00:04:06 +0500

    Убрал комменты в ветке master

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -1,9 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//изменения для теста1
-Console.WriteLine("Hello, World!");
-// изменение для теста 2
-// изменения для теста 3 с удалением
 
-//добавил строчку перевод на русский
+Console.WriteLine("Hello, World!");
 Console.WriteLine("Привет, Мир!"); 
 
